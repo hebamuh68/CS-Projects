2e5926b06335721d9477d908e0fa7d36807edeaf
Heba Allah. Hashim <69214737+hebamuh68@users.noreply.github.com>
2022-05-21 21:21:58 +0200

    Update main.py

--- Operation system simulation/main.py
+++ Operation system simulation/main.py
@@ -7,48 +7,58 @@ obj = main()
 run = True
 while run == True:
     command = list(map(str, input("> ").split()))
+    commands = ["cd", "cls", "dir", "exit", "cd", "rd", "md", "rename", "type", "import",
+                "export"]
 
-    if command[0] == "dir":
-        obj.dir()
+    if command[0] in commands:
 
-    elif command[0] == "import":
-        obj.Import(command[1])
+        if command[0] == "dir":
+            obj.dir()
 
-    elif command[0] == "type":
-        obj.type(command[1])
+        elif command[0] == "import":
+            obj.Import(command[1])
 
-    elif command[0] == "export":
-        source = command[1]
-        destination = command[2]
-        obj.export(source, destination)
+        elif command[0] == "type":
+            obj.type(command[1])
 
-    elif command[0] == "rename":
-        old_name = command[1]
-        new_name = command[2]
-        obj.rename(old_name, new_name)
+        elif command[0] == "export":
+            source = command[1]
+            destination = command[2]
+            obj.export(source, destination)
 
-    elif command[0] == "del":
-        obj.Del(command[1])
+        elif command[0] == "rename":
+            old_name = command[1]
+            new_name = command[2]
+            obj.rename(old_name, new_name)
 
-    elif command[0] == "copy":
-        obj.copy(command[1], command[2])
+        elif command[0] == "del":
+            obj.Del(command[1])
 
-    elif command[0] == "cls":
-        obj.cls()
+        elif command[0] == "copy":
+            obj.copy(command[1], command[2])
 
-    elif command[0] == "md":
-        obj.md(command[1])
+        elif command[0] == "cls":
+            obj.cls()
 
-    elif command[0] == "rd":
-        obj.rd(command[1])
+        elif command[0] == "md":
+            obj.md(command[1])
 
-    elif command[0] == "cd":
-        obj.cd(command[1])
+        elif command[0] == "rd":
+            obj.rd(command[1])
 
-    elif command[0] == "help":
-        obj.help(command[1], command)
+        elif command[0] == "cd":
+            obj.cd(command[1])
+
+        elif command[0] == "help":
+            if len(command) == 1:
+                print("the Command Prompt command that's used to provide more information on another command.")
+            else:
+                obj.help(command[1], command)
 
 
-    elif command[0] == "exit":
-        run = False
-        break
+        elif command[0] == "exit":
+            run = False
+            break
+
+    else:
+        print("command not found! ")
